60c860bf2ad1f918f82da0ae795d2fc5f30e84a8
Argomaru <48087677+Argomaru@users.noreply.github.com>
2022-06-05 16:15:14 +0700

    Add files via upload

---
+++ kk.js
@@ -0,0 +1,33 @@
+//switch case
+// var a = "ayam";
+
+// switch (a) {
+//   case "ayam":
+//     text = "Today is ayam";
+//     console.log(text);
+//     break;
+//   default:
+//     text = "Asu lu semua";
+//     console.log(text);
+
+//   case "kuda":
+//     text = "Today is kuda";
+//     console.log(text);
+// }
+
+let xx = "1234567";
+let yy = "12,34,56,7";
+let res = 0;
+
+function compare(a, b) {
+  console.log(a + b);
+}
+
+do {
+  compare(xx.length, yy.length);
+  xx += 3;
+} while (xx.length < yy.length);
+
+let arr = [1,2,3,4,5,6,7,8,9,10]
+
+let a = arr.map((el) => Ma)
---
+++ latihanis.js
@@ -0,0 +1,73 @@
+// //switch case
+// var bulan = 12;
+// var month;
+
+// switch (bulan) {
+//   case 1:
+//     month = "January";
+//     break;
+//   case 2:
+//     month = "February";
+//     break;
+//   case 3:
+//     month = "March";
+//     break;
+//   case 4:
+//     month = "April";
+//     break;
+//   case 5:
+//     month = "May";
+//     break;
+//   case 6:
+//     month = "June";
+//     break;
+//   case 7:
+//     month = "July";
+//     break;
+//   case 8:
+//     month = "August";
+//     break;
+//   case 9:
+//     month = "September";
+//     break;
+//   case 10:
+//     month = "October";
+//     break;
+//   case 11:
+//     month = "November";
+//     break;
+//   case 12:
+//     month = "December";
+//     break;
+// }
+// console.log(month);
+
+// //While, Do while
+// let cart = "";
+// let chackout = 0;
+// let duit = 100;
+// let uangSekarang = duit;
+// // do {
+// //   cart += "asu";
+// //   chackout += 120;
+// //   duit -= chackout;
+// // } while (chackout <= duit);
+// // console.log(chackout);
+// // console.log(duit);
+// if (chackout <= duit) {
+//   cart += "asu";
+//   chackout += 120;
+//   uangSekarang = duit - chackout;
+// }
+// console.log(chackout);
+// console.log(duit);
+// console.log(uangSekarang);
+
+// const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// fruits.pop();
+// fruits.shift();
+// fruits.unshift("Grape");
+// const citrus = fruits.slice(undefined, 5);
+// console.log(citrus);
+
+console.log(Boolean(-1));
